c32881d4be1c06677b153cbbe237127bc7806ae9
Gert Wijsman <gert@neo4j.fritz.box>
2024-10-07 22:09:36 +0200

    add txt and neo4j start

---
+++ do-export-to-txt.py
@@ -0,0 +1,52 @@
+#
+# export all issues from odoo with message to txt files
+# for usage of GenAI-Stack
+# export attachments to a folder
+#
+# Gert Wijsman
+# August 2024
+#
+# inspriation:
+# https://www.odoo.com/documentation/8.0/api_integration.html
+#
+
+import os
+import logging
+from dotenv import load_dotenv
+from odoo.OdooInfo import OdooInfo
+from odoo.OdooIssues import OdooIssues
+from odoo.OdooIssue import OdooIssue
+from ImportLogging import setup_logging 
+
+logger = logging.getLogger(__name__)
+
+def main():
+    load_dotenv()
+    setup_logging()
+    logger.info("Starting ODOO Export...")
+    
+    db = os.getenv('ODOO_DB')
+    username = os.getenv('ODOO_USERNAME')
+    password = os.getenv('ODOO_PASSWORD')
+    
+    host = os.getenv('ODOO_HOST')
+    url = "https://" + host + "/xmlrpc/2/"
+
+    outputfolder = os.getenv('TEXT_OUTPUT_FOLDER')
+    
+    odoo_info = OdooInfo(db, username, password, host)
+    logger.debug("DB connection: %s", odoo_info)
+    
+    odoo_issues = OdooIssues(odoo_info)
+    
+    for issue in odoo_issues: 
+        logger.debug(issue)
+        # issue.debug_dump(False)
+        # issue.debug_dump()
+        issue.write_to_text_file(outputfolder, True)
+        # exit()
+        break 
+    logger.info("ODOO Export done.")
+        
+if __name__ == "__main__":
+    main()
